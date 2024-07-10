ca4a6ec52119b77f04c45814628ccf855a99ddc7
陈阳 <954634191@qq.com>
2024-07-10 19:14:34 +0800

    用户37的20240710第1次提交

--- src/main/java/cn/doitedu/flink/utils/SQLUtilsV2.java
+++ src/main/java/cn/doitedu/flink/utils/SQLUtilsV2.java
@@ -2,7 +2,7 @@ package cn.doitedu.flink.utils;
 
 public class SQLUtilsV2 {
     public static void main(String[] args) {
-        System.out.println(333);
+        System.out.println(666);
 
     }
 }
